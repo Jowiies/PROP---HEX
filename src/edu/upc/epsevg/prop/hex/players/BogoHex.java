c933c6c2a0491a023bfe62588be0db0d5267a2ad
maryupc <mary.ojuyenum@estudiantat.upc.edu>
2024-12-31 03:43:00 +0100

    zobrist-

--- src/edu/upc/epsevg/prop/hex/players/BogoHex.java
+++ src/edu/upc/epsevg/prop/hex/players/BogoHex.java
@@ -2,6 +2,9 @@ package edu.upc.epsevg.prop.hex.players;
 
 import edu.upc.epsevg.prop.hex.*;
 import edu.upc.epsevg.prop.hex.algorithms.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Implementació del jugador BogoHex per al joc de Hex.
@@ -11,6 +14,9 @@ public class BogoHex implements IPlayer, IAuto
 {
     private final String name = "BogoHexPlayer";
     private boolean ids;
+    private static final int BOARD_SIZE = 7; //luego mirar
+    private static final long[][][] zobristTable = buildZobristTable();
+    private static final Map<Long, Integer> transpositionTable = new HashMap<>();
     MiniMax miniMaxAlgorithm;
 	
 	/**
@@ -46,6 +52,8 @@ public class BogoHex implements IPlayer, IAuto
     @Override
     public PlayerMove move(HexGameStatus hexGameStatus)
     {
+        long zobristHash = calculateZobristHash(hexGameStatus); //-- > le pasamos el tablero del juego, para calcular el hash principal
+        //int score = minimax(board, zobristHash, 3, true);
         return new PlayerMove(
                 miniMaxAlgorithm.findBestMove(hexGameStatus),
                 miniMaxAlgorithm.getExplorationDepth(),
@@ -72,6 +80,31 @@ public class BogoHex implements IPlayer, IAuto
     public String getName()
     {
         return this.name;
+    }
+    
+    public static long[][][] buildZobristTable() {
+        long[][][] table = new long[2][BOARD_SIZE][BOARD_SIZE];
+        Random random = new Random();
+        for (int player = 0; player < 2; player++) {
+            for (int x = 0; x < BOARD_SIZE; x++) {
+                for (int y = 0; y < BOARD_SIZE; y++) {
+                    table[player][x][y] = random.nextLong();
+                }
+            }
+        }
+        return table;
+    }
+
+    public static long calculateZobristHash(HexGameStatus gs) {
+        long hash = 0L;
+        for (int x = 0; x < gs.getSize(); x++) {
+            for (int y = 0; y < gs.getSize(); y++) {
+                if (gs.getPos(x, y) != 0) {
+                    hash ^= zobristTable[gs.getPos(x, y)- 1][x][y];
+                }
+            }
+        }
+        return hash;
     }
 
 }
